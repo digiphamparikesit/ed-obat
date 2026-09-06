e9a9c34d3edee50e58d5577e7f7bfff32c3528dd
digiphamparikesit <ifrsparikesit@gmail.com>
2026-09-06 17:13:02 +0800

    index.html

--- js/config.js
+++ js/config.js
@@ -1,5 +1,2 @@
-// config.js
-const SUPABASE_URL = 'https://bfpzfzjaumnyukwzlevq.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://bfpzfzjaumnyukwzlevq.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_3UijJQbb-6dFQKM8u10q3g_DKzk8Vyt';
-
-// Ganti dengan URL dan anon key dari proyek Supabase Anda.
